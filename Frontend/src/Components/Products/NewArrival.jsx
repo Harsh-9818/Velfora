dcb091b83464c13b92a590fda46d661b572b5bee
Harsh Tanwar <harsh.tanwar9818@gmail.com>
2025-06-02 16:13:23 +0530

    Complete Home Page

--- Frontend/src/Components/Products/NewArrival.jsx
+++ Frontend/src/Components/Products/NewArrival.jsx
@@ -1,5 +1,15 @@
+import { useEffect, useRef, useState } from 'react';
 import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
+import { Link } from 'react-router-dom';
 const NewArrival = () => {
+
+  const scrollRef = useRef(null);
+  const [isDragging, setIsDragging] = useState(false);
+  const [startX, setStartX] = useState(0);
+  const [scrollLeft, setScrollLeft] = useState(false);
+  const [canScrollLeft, setCanScrollLeft] = useState(false);
+  const [canScrollRight, setCanScrollRight] = useState(true);
+
   const newArrival = [
     {
       _id: "1",
@@ -10,6 +20,7 @@ const NewArrival = () => {
           altText: "Stylish Jacket",
         },
       ],
+      price: "200",
     },
     {
       _id: "2",
@@ -20,6 +31,7 @@ const NewArrival = () => {
           altText: "Stylish Jacket",
         },
       ],
+      price: "200",
     },
     {
       _id: "3",
@@ -30,6 +42,7 @@ const NewArrival = () => {
           altText: "Stylish Jacket",
         },
       ],
+      price: "200",
     },
     {
       _id: "4",
@@ -40,6 +53,7 @@ const NewArrival = () => {
           altText: "Stylish Jacket",
         },
       ],
+      price: "200",
     },
     {
       _id: "5",
@@ -50,6 +64,7 @@ const NewArrival = () => {
           altText: "Stylish Jacket",
         },
       ],
+      price: "200",
     },
     {
       _id: "6",
@@ -60,6 +75,7 @@ const NewArrival = () => {
           altText: "Stylish Jacket",
         },
       ],
+      price: "200",
     },
     {
       _id: "7",
@@ -70,11 +86,56 @@ const NewArrival = () => {
           altText: "Stylish Jacket",
         },
       ],
+      price: "200",
     },
   ];
 
+  const handleMouseDown = (e) => {
+    setIsDragging(true);
+    setStartX(e.pageX - scrollRef.current.offsetLeft)
+    setScrollLeft(scrollRef.current.scrollLeft);
+  } 
+
+  const handleMouseMove = (e) => {
+    if (!isDragging) return;
+    const x = e.pageX - scrollRef.current.offsetLeft;
+    const walk = x - startX;
+    scrollRef.current.scrollLeft = scrollLeft - walk;
+  } 
+
+  const handleMouseUpOrLeave = () => {
+    setIsDragging(false);
+  }
+
+  const scroll = (direction) => {
+    const scrollAmount = direction === "left" ? -300 : 300;
+    scrollRef.current.scrollBy({left: scrollAmount, behaviour: "smooth"});
+  }
+
+  // Update scroll button 
+  const updateScrollButtons = () => {
+    const container = scrollRef.current;
+
+    if( container ){
+      const leftScroll = container.scrollLeft;
+      const rightScrollable = container.scrollWidth > leftScroll + container.clientWidth;
+
+      setCanScrollLeft(leftScroll > 0);
+      setCanScrollRight(rightScrollable);
+    }
+  }
+
+  useEffect(() => {
+    const container = scrollRef.current;
+    if( container ) {
+      container.addEventListener("scroll", updateScrollButtons);
+      updateScrollButtons();
+      return () => container.removeEventListener("Scroll", updateScrollButtons);
+    }
+  },[])
+
   return (
-    <section>
+    <section className='py-16 px-4 lg:px-0'>
         <div className=" container mx-auto text-center mb-10 relative">
             <h2 className="text-3xl font-bold mb-4">Explore New Arrivals</h2>
             <p className="text-ld text-gray-600 mb-8">
@@ -84,17 +145,54 @@ const NewArrival = () => {
 
             {/* Scroll buttons */}
             <div className="absolute right-0 bottom-[-30px] flex space-x-2">
-                <button className="p-2 rounded-full border bg-white text-black">
-                    <FiChevronLeft className="text-2xl"/>
+                <button 
+                onClick={() => scroll("left")}
+                disabled={!canScrollLeft}
+                className={`p-2 rounded-full border ${
+                  canScrollLeft ? " bg-white text-black" : "bg-gray-200 text-gray-400 cursor-not-allowed"
+                }`}>
+                    <FiChevronLeft className="text-xl"/>
                 </button>
-                <button className="p-2 rounded-full border bg-white text-black">
-                    <FiChevronRight className="text-2xl"/>
+                <button 
+                onClick={() => scroll("right")}
+
+                className={`p-2 rounded-full border ${
+                  canScrollRight ? " bg-white text-black" : "bg-gray-200 text-gray-400 cursor-not-allowed"
+                }`}>
+                    <FiChevronRight className="text-xl"/>
                 </button>
             </div>
         </div>
 
         {/* Scrollable Content */}
-
+        <div 
+        ref={scrollRef} 
+        className={`container mx-auto overflow-x-scroll flex space-x-6 relative ${isDragging ? "cursor-grabbing" : "cursor-grab"}`}
+        onMouseDown={handleMouseDown}
+        onMouseMove={handleMouseMove}
+        onMouseUp={handleMouseUpOrLeave}
+        onMouseLeave={handleMouseUpOrLeave}
+        >
+          {newArrival.map((product) => (
+            <div 
+              key={product._id}
+              className="min-w-[100%] sm:min-w-[50%] lg:min-w-[30%] relative"
+            >
+              <img
+                src={product.image[0]?.url}
+                alt={product.image[0]?.altText || product.name}
+                className='w-full h-[400px] object-cover rounded-lg'
+                draggable="false"
+              />
+              <div className="absolute bottom-0 left-0 right-0 bg-opacity-50 backdrop-blur-md text-white p-4 rounded-b-lg">
+                <Link to={`/product/${product._id}`} className="block"> 
+                  <h4 className='font-medium'>{product.name}</h4>
+                  <p className='mt-1'>${product.price}</p>
+                </Link>
+              </div>
+            </div>
+          ))}
+        </div>
     </section>
   )
 };
